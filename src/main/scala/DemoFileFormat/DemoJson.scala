f4e090b3b2ec52962b766d1498ec0df61dc29e2f
Ravi Verma3 <RV00451128@TechMahindra.com>
2019-07-24 12:48:46 +0530

    json format

--- src/main/scala/DemoFileFormat/DemoJson.scala
+++ src/main/scala/DemoFileFormat/DemoJson.scala
@@ -40,10 +40,19 @@ object DemoJson {
     val student: DataFrame =sc.read.option("multiline","true").json("D:\\mypro\\spark\\student.json")
     println(student.printSchema())
 
-    val add=student.withColumn("address",explode(array("address"))).select("address.city","address.state").show(truncate = false)
+    val add=student.withColumn("address",explode(array("address"))).select("address.city","address.state")
+    add.show()
 
 
-    student.select(explode(student("phoneNumbers"))).show
+   /* Flattening structs
+      A star (*) can be used to select all of the subfields in a struct.*/
+
+    var dfPhoneNumber=student.select(explode(student("phoneNumbers"))).toDF("phonenumber")
+    var dfNumber=dfPhoneNumber.select("phonenumber.number","phonenumber.type")
+    var dfNumber1=dfPhoneNumber.select("phonenumber.*")
+
+    dfNumber.show()
+    dfNumber1.show()
 
     student.createOrReplaceTempView("student")
     sc.sql("select explode(phoneNumbers) from student").show()
